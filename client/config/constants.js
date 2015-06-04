49572fc60ad5aa5a97765028e74b9f2150321bda
Edwin Dover <edsdover@gmail.com>
2015-06-04 16:56:45 -0700

    test

--- client/config/constants.js
+++ client/config/constants.js
@@ -2,4 +2,4 @@
 
 angular.module('f1-index')
 .constant('firebaseUrl', 'https://facebooknode.firebaseio.com/')
-.constant('nodeUrl', 'http://198.199.97.62/f1-index');
+.constant('nodeUrl', 'https://pacific-bayou-6427.herokuapp.com');
